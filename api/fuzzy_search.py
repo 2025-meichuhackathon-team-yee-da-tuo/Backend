ec8d2859cc2160a78a401cfe34509607cc54ff6c
timchen <timchen0105@gmail.com>
2025-09-21 00:13:55 +0800

    Delete: put query at the first of the results

--- api/fuzzy_search.py
+++ api/fuzzy_search.py
@@ -105,8 +105,6 @@ def _fuzzy_search_core(query: str, items: List[str], top_k: int, min_score: floa
             break
         if float(sims[idx]) < min_score:
             continue
-        if query not in results:
-            results.insert(0, query)
         results.append(items[idx])
     return results
 
